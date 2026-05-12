bc7a5b435f5d062af65094ddce1aab5b2def579f
Faustin <faustinbertrand1990@gmail.com>
2026-05-12 13:09:11 +0200

    fix(a11y): unblock viewport zoom

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -65,8 +65,6 @@ export const metadata: Metadata = createMetadata("en");
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export default function RootLayout({
